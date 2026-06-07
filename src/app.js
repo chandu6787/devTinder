c4e5088c3fe18e9ffaf921a32edd4af9b051ce64
Chandu <chandugarapati789@gmail.com>
2026-06-07 16:38:03 +0530

    fixed cors origin issue

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const { UserAuth } = require("./middlewares/auth");
 const cors = require("cors");
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173","https://devtinder-2-uc00.onrender.com"],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
